b5c58ee724680f074cc630878bfcbfa6d4650c2a
XPS-8500\Armando <armando.castillo@sundevs.com>
2018-06-09 16:47:34 -0500

    Created the base class for Model Training

---
+++ app/src/main/java/com/armandoncm/opencvfacerecognitionexample/faceRecognition/ModelTraining.java
@@ -0,0 +1,19 @@
+package com.armandoncm.opencvfacerecognitionexample.faceRecognition;
+
+import org.opencv.core.Mat;
+
+public class ModelTraining {
+
+    /**
+     * This method uses the array of cropped faces in the form of OpenCV Matrix (Mat)
+     * to train the face recognition model of a given person
+     * @param label Label to whom the model will identify the given faces
+     * @param trainingFaces Array of OpenCV Matrices (Mat) whose face will be used to train
+     *                      the model
+     */
+    public void trainModel(int label, Mat[] trainingFaces){
+
+        // TODO Implement
+    }
+
+}
